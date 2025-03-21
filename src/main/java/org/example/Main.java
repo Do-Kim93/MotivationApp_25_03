054ce7520383086eb8781412189d2281430db020
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-21 11:17:53 +0900

    add 까지 완성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-            System.out.println("i = asdfasdfasdf" );
-        }
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
     }
+}
