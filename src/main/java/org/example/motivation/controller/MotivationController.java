8a47c09c32f0f16c6f075b0fb1f7e399eb1ef81e
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-24 13:20:58 +0900

    update 구현 강사님ver

--- src/main/java/org/example/motivation/controller/MotivationController.java
+++ src/main/java/org/example/motivation/controller/MotivationController.java
@@ -100,42 +100,64 @@ public class MotivationController {
 
     }
 
-    public void update(String cmd) {
-        int id = Integer.parseInt(cmd.split(" ")[1]);
-
-        Motivation foundMotivation = null;
-
-        for (int i = 0; i < motivations.size(); i++) {
-            Motivation motivation = motivations.get(i);
-            if (motivation.getId() == id) {
-                foundMotivation = motivation;
-                break;
-            }
+    public void edit(String cmd) {
+        int id;
+        try {
+            id = Integer.parseInt(cmd.split(" ")[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("명령어 확인해라");
+            return;
         }
+
+        Motivation foundMotivation = findById(id);
 
         if (foundMotivation == null) {
             System.out.println("해당 moti는 없던데????");
             return;
         }
-        System.out.printf("기존 motivation : %s\n", foundMotivation.getBody());
-        System.out.printf("기존 source : %s\n", foundMotivation.getSource());
-        System.out.print("body : ");
-        String body = Container.getScanner().nextLine();
-        System.out.print("source : ");
-        String source = Container.getScanner().nextLine();
-        if (body.isEmpty()&&source.isEmpty()) {
-            System.out.println("변경 사항이 없습니다.");
-            return;
-        }else if (body.isEmpty()){
-            foundMotivation.setSource(source);
-        }else if (source.isEmpty()){
-            foundMotivation.setBody(body);
-        }else {
-            foundMotivation.setBody(body);
-            foundMotivation.setSource(source);
+
+        // 찾은 motivation의 인스턴스 변수에 접근
+        System.out.println("body(기존) : " + foundMotivation.getBody());
+        System.out.println("source(기존) : " + foundMotivation.getSource());
+
+        String newBody;
+        String newSource;
+        // 수정사항 입력받기
+        while (true) {
+            System.out.print("new body : ");
+            newBody = Container.getScanner().nextLine().trim();
+
+            if (newBody.length() != 0) {
+                break;
+            }
+            System.out.println("수정사항(body) 입력해");
         }
 
+        while (true) {
+            System.out.print("new source : ");
+            newSource = Container.getScanner().nextLine();
 
-        System.out.println(id + "번 moti 변경됨");
+            if (newSource.length() != 0) {
+                break;
+            }
+            System.out.println("수정사항(source) 입력해");
+        }
+
+        // 찾은 motivation의 인스턴스 변수 값 수정
+        foundMotivation.setBody(newBody);
+        foundMotivation.setSource(newSource);
+
+        System.out.println(id + "번 moti 수정됨");
+
+    }
+
+    // 명령어의 id 와 일치하는 motivation 찾기
+    private Motivation findById(int id) {
+        for (Motivation motivation : motivations) {
+            if (motivation.getId() == id) {
+                return motivation;
+            }
+        }
+        return null;
     }
 }
